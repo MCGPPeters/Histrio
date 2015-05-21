8eb546d6d7e2995f0c0b227cc1709c4f773f9789
Maurice CGP Peters <mcgppeters@hotmail.com>
2015-05-21 17:18:11 +0200

    Package restore added

--- src/Histrio.Build/Build.ps1
+++ src/Histrio.Build/Build.ps1
@@ -4,6 +4,12 @@
     [string]$nugetSource = "http://www.nuget.org/api/v2"
     )
 
+$rootDir                = Resolve-Path ..\..\
+$srcDir                 = "$rootDir\src"
+$packagesDir            = "$srcDir\packages"
+$solutionFilePath       = "$srcDir\$projectName.sln"
+$nugetPath              = FindTool "NuGet.CommandLine.*\tools\nuget.exe" "$packagesDir"
+
 if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
     $buildNumber = [int]$Env:APPVEYOR_BUILD_NUMBER
     Write-Output "Using APPVEYOR_BUILD_NUMBER"
@@ -11,10 +17,21 @@ if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
 
 "Build number $buildNumber"
 
-
+exec { .$nugetPath restore src\Histrio.sln }
 
 Import-Module .\src\packages\psake.4.4.2\tools\psake.psm1
 
 Invoke-Psake .\default.ps1 $task -framework "4.5.1x64" -properties @{ buildNumber=$buildNumber; nugetSource=$nugetSource }
 
 Remove-Module psake
+
+function FindTool {
+    param(
+        [string]$name,
+        [string]$packageDir
+    )
+
+    $result = Get-ChildItem "$packageDir\$name" | Select-Object -First 1
+
+    return $result.FullName
+}
