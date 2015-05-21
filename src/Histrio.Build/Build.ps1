2a32730791a9decb8cc37f3c21db1fe54655dca5
Maurice CGP Peters <mcgppeters@hotmail.com>
2015-05-21 22:22:21 +0200

    the horror... the horror

--- src/Histrio.Build/Build.ps1
+++ src/Histrio.Build/Build.ps1
@@ -4,24 +4,6 @@
     [string]$nugetSource = "http://www.nuget.org/api/v2"
     )
 
-function FindTool {
-    param(
-        [string]$name,
-        [string]$packageDir
-    )
-
-    $result = Get-ChildItem "$packageDir\$name" | Select-Object -First 1
-
-    return $result.FullName
-}
-
-	$rootDir                = Resolve-Path ..\..\
-	$srcDir                 = "$rootDir\src"
-	$toolsDir               = "$rootDir\tools"
-	$packagesDir            = "$srcDir\packages"
-	$solutionFilePath       = "$srcDir\$projectName.sln"
-	$nugetPath              = "$toolsDir\nuget.exe"
-
 if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
     $buildNumber = [int]$Env:APPVEYOR_BUILD_NUMBER
     Write-Output "Using APPVEYOR_BUILD_NUMBER"
@@ -29,7 +11,7 @@ if(Test-Path Env:\APPVEYOR_BUILD_NUMBER){
 
 "Build number $buildNumber"
 
-Invoke-Command { .$nugetPath restore $srcDir\Histrio.sln }
+..\..\tools\Nuget.exe restore ..\..\src\Histrio.sln
 
 Import-Module .\src\packages\psake.4.4.2\tools\psake.psm1
 
