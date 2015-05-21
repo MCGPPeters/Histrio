1ae1f7c167487bef4763643f6a65a0649b9db2e5
Maurice CGP Peters <mcgppeters@hotmail.com>
2015-05-21 16:18:37 +0200

    Build scripts and merging fixes #9

---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("Histrio")]
+[assembly: AssemblyCopyright("Copyright © Maurice CGP Peters 2015")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
